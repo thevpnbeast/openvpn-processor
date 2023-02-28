a682b247af6846b41a8e6abf6c7cc4c4298378b1
bilalcaliskan <bilalcaliskan@protonmail.com>
2023-02-28 23:58:34 +0300

    refactor: comment out database initialization

--- src/internal/options/options.go
+++ src/internal/options/options.go
@@ -1,9 +1,5 @@
 package options
 
-import (
-	"log"
-)
-
 const (
 	ApplicationName      = "openvpn-processor"
 	TargetConfigFileName = "application"
@@ -13,15 +9,15 @@ const (
 var opts *OpenvpnProcessorOptions
 
 func init() {
-	applicationId := getStringEnv("CONFIG_APPLICATION_ID", "jl9vegs")
-	confProfileId := getStringEnv("CONFIG_PROFILE_ID", "hek207s")
+	//applicationId := getStringEnv("CONFIG_APPLICATION_ID", "jl9vegs")
+	//confProfileId := getStringEnv("CONFIG_PROFILE_ID", "hek207s")
 
 	opts = newOpenvpnProcessorOptions()
 
-	if err := fetchConfig(applicationId, confProfileId); err != nil {
-		log.Printf("FATAL: fatal error occurred while fetching config from AWS AppConfig (error=%s)", err.Error())
-		panic(err)
-	}
+	//if err := fetchConfig(applicationId, confProfileId); err != nil {
+	//	log.Printf("FATAL: fatal error occurred while fetching config from AWS AppConfig (error=%s)", err.Error())
+	//	panic(err)
+	//}
 }
 
 // OpenvpnProcessorOptions represents openvpn-processor environment variables
